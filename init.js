32f43c74b764d02561ccd5a849671a6a71d912a9
Pierre Gillon <gillon.pierre69@gmail.com>
2018-12-27 18:27:30 +0700

    Initialize pre-existing highlight on document loaded.

---
+++ init.js
@@ -0,0 +1,27 @@
+(function(){
+    'use strict';
+
+    var paragraphSelector = "#gc-pagecontent > article > div > section:nth-child(3) > p:nth-child(2)";
+    var existingRange = getRange(paragraphSelector, 2, 2, 2, 20);
+    if (existingRange && existingRange.startOffset < existingRange.endOffset) {
+        highlight(existingRange);
+    }
+
+    function highlight(range) {
+        var span = document.createElement('SPAN');
+        span.appendChild(range.extractContents());
+        span.style.background = "red";
+        range.insertNode(span);
+    }
+
+    function getRange(paragraphSelector, textNodeStartIndex, textNodeEndIndex, offsetStart, offsetEnd) {
+        var paragraph = document.querySelector(paragraphSelector);
+        var textNodeStart = paragraph.childNodes[textNodeStartIndex];
+        var textNodeEnd = paragraph.childNodes[textNodeEndIndex];
+        var range = new Range();
+        range.setStart(textNodeStart, offsetStart);
+        range.setEnd(textNodeEnd, offsetEnd);
+        return range;
+    }
+    
+})();
