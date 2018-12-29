9a137f7878412ecba9f4bb18274133b7c6731c7a
Pierre Gillon <gillon.pierre69@gmail.com>
2018-12-29 09:56:20 +0700

    Update code to save ranges in localstorage.

--- browser/js/highlight-selected-text.js
+++ browser/js/highlight-selected-text.js
@@ -1,9 +1,38 @@
 (function(){
     'use strict';
-
+    
     var range = document.getSelection().getRangeAt(0);
-    if (range && range.startOffset < range.endOffset) {
+    if (range) {
+        var rangeData = JSON.parse(localStorage.getItem('ranges') || '[]');
+        rangeData.push({
+            paragraphSelector: getQuerySelector(range.commonAncestorContainer.parentElement), 
+            textNodeStartIndex: getChildNodeIndex(range.commonAncestorContainer.parentElement, range.startContainer), 
+            textNodeEndIndex: getChildNodeIndex(range.commonAncestorContainer.parentElement, range.endContainer), 
+            offsetStart: range.startOffset, 
+            offsetEnd: range.endOffset
+        });
+        localStorage.setItem('ranges', JSON.stringify(rangeData));
         highlight(range, "orange");
+    }
+
+    function getChildNodeIndex(container, node) {
+        for(var i=0; i < container.childNodes.length; i++) {
+            if(container.childNodes[i] == node) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function getQuerySelector(element) {
+        if(!element || !element.tagName || element.tagName === "HTML") {
+            return undefined;
+        }
+        var parentQuerySelector = getQuerySelector(element.parentElement);
+        if(parentQuerySelector) {
+            return parentQuerySelector + " > " + element.tagName.toLowerCase();
+        }
+        return element.tagName.toLowerCase();
     }
     
     function highlight(range, color) {
--- browser/js/init.js
+++ browser/js/init.js
@@ -1,16 +1,30 @@
 (function(){
     'use strict';
 
-    var paragraphSelector = "#gc-pagecontent > article > div > section:nth-child(3) > p:nth-child(2)";
-    var existingRange = getRange(paragraphSelector, 2, 2, 2, 20);
-    if (existingRange && existingRange.startOffset < existingRange.endOffset) {
-        highlight(existingRange, "red");
+    var rangeData = JSON.parse(localStorage.getItem('ranges') || '[]');
+    if(rangeData) {
+        rangeData.forEach(data => {
+            var range = getRange(
+                data.paragraphSelector, 
+                data.textNodeStartIndex, 
+                data.textNodeEndIndex, 
+                data.offsetStart, 
+                data.offsetEnd);
+            
+            if (range) {
+                highlight(range, "red");
+            }
+        });
+        
     }
 
     function getRange(paragraphSelector, textNodeStartIndex, textNodeEndIndex, offsetStart, offsetEnd) {
-        var paragraph = document.querySelector(paragraphSelector);
-        var textNodeStart = paragraph.childNodes[textNodeStartIndex];
-        var textNodeEnd = paragraph.childNodes[textNodeEndIndex];
+        var domElement = document.querySelector(paragraphSelector);
+        if(!domElement) {   
+            return undefined;
+        }
+        var textNodeStart = domElement.childNodes[textNodeStartIndex];
+        var textNodeEnd = domElement.childNodes[textNodeEndIndex];
         var range = new Range();
         range.setStart(textNodeStart, offsetStart);
         range.setEnd(textNodeEnd, offsetEnd);
