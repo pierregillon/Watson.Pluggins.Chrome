897e8407ea4896674218002652356c07bc94a2b0
Pierre Gillon <gillon.pierre69@gmail.com>
2019-01-07 20:40:17 +0700

    Add badge to count numbers of suspicious facts

--- background.js
+++ background.js
@@ -2,7 +2,8 @@ var repository = new FactRepository(new HttpClient("http://localhost:5000"));
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if(changeInfo.status == "complete") {
-        repository.getSuspiciousFacts(tab.url).then(function (suspiciousFacts) {
+        repository.getSuspiciousFacts(tab.url).then(suspiciousFacts => {
+            updateBadge(tabId, suspiciousFacts.length);
             chrome.tabs.sendMessage(tabId, {
                 type: "suspiciousFactsLoaded",
                 suspiciousFacts: suspiciousFacts
@@ -13,8 +14,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 chrome.runtime.onMessage.addListener(function(message, sender, reply) {
     if(message.type == "reportNewSuspiciousFact") {
-        chrome.tabs.query({active: true}, function(tabs) {
-            repository.report(tabs[0].url, message.newSuspiciousFact).then(function () {
+        chrome.tabs.query({active: true}, tabs => {
+            repository.report(tabs[0].url, message.newSuspiciousFact).then(() => {
+                chrome.browserAction.getBadgeText({tabId: tabs[0].id}, text => {
+                    updateBadge(tabs[0].id, parseInt(text) + 1);
+                });
                 chrome.tabs.sendMessage(tabs[0].id, {
                     type: "suspiciousFactsLoaded",
                     suspiciousFacts: [toReadModel(message.newSuspiciousFact)]
@@ -24,6 +28,17 @@ chrome.runtime.onMessage.addListener(function(message, sender, reply) {
     }
     reply();
 });
+
+function updateBadge(tabId, count) {
+    chrome.browserAction.setBadgeText({
+        text: count.toString(),
+        tabId: tabId
+    });
+    chrome.browserAction.setBadgeBackgroundColor({
+        color: 'red',
+        tabId: tabId
+    });
+}
 
 function toReadModel(fact) {
     return {
