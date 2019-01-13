d77e57718bbb2f0300d015e15a383ce71ca6be15
Pierre Gillon <gillon.pierre69@gmail.com>
2019-01-13 21:53:03 +0700

    Create an overlay following mouse on highlighted fact.

--- browser/js/init.js
+++ browser/js/init.js
@@ -6,7 +6,7 @@
             msg.suspiciousFacts.forEach(fact => {
                 var textRange = createTextRange(fact);
                 if (textRange) {
-                    highlight(textRange, "red");
+                    highlight(textRange);
                 }
             });
             sendResponse();
@@ -40,10 +40,68 @@
         }
     }
 
-    function highlight(range, color) {
-        var span = document.createElement('SPAN');
-        span.appendChild(range.extractContents());
-        span.className = "highlight";
-        range.insertNode(span);
+    // ----- Overlay
+
+    var overlay = new Overlay();
+
+    function highlight(range) {
+        var element = createHighlight();
+        element.appendChild(range.extractContents());
+        range.insertNode(element);
     }
+
+    function createHighlight() {
+        var element = document.createElement('SPAN');
+        element.className = "highlight";
+        element.onmouseenter = mouseEnterFact;
+        element.onmouseleave = mouseLeaveFact;
+        element.onmousemove = mouseMoveOnFact;
+        return element;
+
+        function mouseEnterFact(event) {
+            this.overlaying = true;
+            overlay.show(event.pageX, event.pageY);
+        }
+    
+        function mouseLeaveFact() {
+            this.overlaying = false;
+            overlay.hide();
+        }
+    
+        function mouseMoveOnFact(event) {
+            if (this.overlaying) {
+                overlay.show(event.pageX, event.pageY);
+            }
+        }
+    }
+
+    function Overlay() {
+        var self = this;
+        var domElement = null;
+    
+        self.show = function(x, y) {
+            if (!domElement) {
+                domElement = createDomElement();
+                document.body.append(domElement);
+            }
+            domElement.style.left = (x + 5) + "px";
+            domElement.style.top = (y - 35) + "px";
+        }
+
+        self.hide = function() {
+            if(domElement) {
+                document.body.removeChild(domElement);
+                domElement = null;
+            }
+        }
+
+        function createDomElement() {
+            var overlay = document.createElement('SPAN');
+            var imageUrl = chrome.runtime.getURL('extension/images/icon-detective16.png');
+            overlay.innerHTML = "<img src=\"" + imageUrl + "\"> This fact has been reported as <strong>suspicious</strong> by a user.";
+            overlay.className = "info-overlay";
+            return overlay;        
+        }
+    }
+
 })();
