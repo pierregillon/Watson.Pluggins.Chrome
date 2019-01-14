bdd559cbdbedd441481578b70420b22a244f01d7
Pierre Gillon <gillon.pierre69@gmail.com>
2019-01-14 22:41:09 +0700

    Ignore specific web site with non static content.

--- background.js
+++ background.js
@@ -1,18 +1,27 @@
 var repository = new FactRepository(new HttpClient("http://localhost:5000"));
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(changeInfo.status == "complete") {
-        repository.getSuspiciousFacts(tab.url)
-            .then(suspiciousFacts => {
-                updateBadge(tabId, suspiciousFacts.length);
-                chrome.tabs.sendMessage(tabId, {
-                    type: "suspiciousFactsLoaded",
-                    suspiciousFacts: suspiciousFacts
+    if (changeInfo.status == "loading") {
+        if (isUrlAccepted(tab.url) == false) {
+            disableExtension(tabId);
+        }
+        else {
+            enableExtension(tabId);
+        }
+    }
+    else if (changeInfo.status == "complete") {
+        if (isUrlAccepted(tab.url)) {
+            repository.getSuspiciousFacts(tab.url)
+                .then(suspiciousFacts => {
+                    updateBadge(tabId, suspiciousFacts.length);
+                    chrome.tabs.sendMessage(tabId, {
+                        type: "suspiciousFactsLoaded",
+                        suspiciousFacts: suspiciousFacts
+                    });
+                }).catch(err => {
+                    disableExtension(tabId);
                 });
-            }).catch(err => {
-                chrome.browserAction.setIcon({tabId: tabId, path: "extension/images/icon-detective16-disabled.png"});
-                chrome.browserAction.disable(tabId);
-            });
+        }
     }
 });
 
@@ -39,4 +48,41 @@ function updateBadge(tabId, count) {
         color: 'red',
         tabId: tabId
     });
+}
+
+function enableExtension(tabId) {
+    chrome.browserAction.enable(tabId);
+    chrome.browserAction.setIcon({tabId: tabId, path: "extension/images/icon-detective16.png"});
+}
+
+function disableExtension(tabId) {
+    chrome.browserAction.setIcon({tabId: tabId, path: "extension/images/icon-detective16-disabled.png"});
+    chrome.browserAction.disable(tabId);
+}
+
+function isUrlAccepted(url) {
+    var domain = getDomain(url);
+    switch (domain) {
+        case undefined:
+        case "google":
+        case "facebook":
+        case "booking":
+        case "airbnb":
+        case "twitter":
+        case "instagram":
+            return false;
+        default:
+            return true;
+    }
+}
+
+function getDomain(url) {
+    var url = new URL(url);
+    var hostParts = url.hostname.split(".");
+    if (url.hostname.startsWith("www")) {
+        return hostParts[1].toLowerCase();
+    }
+    else {
+        return hostParts[0].toLowerCase();
+    }
 }
